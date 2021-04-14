588ec6a9b024fe2e4154261819ee685dd7607faa
februar-y <jarmah241@gmail.com>
2021-04-14 14:20:22 -0400

    binary search sol

--- easy/SearchInsert.js
+++ easy/SearchInsert.js
@@ -5,9 +5,26 @@
  * @return {number}
  */
 
+// Binary Search Solution !!
+// O(logn)
+var searchInsert = function(nums, target) {
+    let start = 0;
+    let end = nums.length-1;
+    let mid;
+    
+    while(start <= end) {
+        mid = Math.floor((start+end)/2);
+        
+        if(nums[mid] === target) return mid;
+        
+        if(nums[mid] > target) end = mid-1;
+        else start = mid+1;
+    }
+    return start;
+};
 
-// Simple solution
 
+// Simple solution O(n)
 /**
  * if currrent number in looop is greater than or equal to target
  * return i (index)
