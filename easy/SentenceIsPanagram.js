aedcaae02311f06a0ea70fe0c8ff267d9930babf
februar-y <jarmah241@gmail.com>
2021-04-28 17:52:41 -0400

    updates easy sol

--- easy/SentenceIsPanagram.js
+++ easy/SentenceIsPanagram.js
@@ -1,6 +1,10 @@
 /**
+ * https://leetcode.com/problems/check-if-the-sentence-is-pangram/
  * 
  * 
+ * A pangram is a sentence where every letter of the English alphabet appears at least once.
+
+Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
  */
 
 // My sol O(n) - time | O(n)
@@ -24,4 +28,10 @@
     }
     
     return true
+};
+
+// Using set
+
+var checkIfPangram = function(sentence) {
+    return new Set(sentence.split("")).size == 26;
 };
