c5cda77b5c0fffb32ec59f3a87706ca5a8d7d424
februar-y <jarmah241@gmail.com>
2021-05-19 21:49:17 -0400

    steps to zero easy

---
+++ easy/StepsToZero.js
@@ -0,0 +1,20 @@
+/*
+https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+
+*/
+
+var numberOfSteps = function(num) {
+    let steps = 0
+    
+    while(num > 0){
+        if(num % 2 === 0){
+            num /= 2
+            steps++
+        } else {
+            num -= 1
+            steps++
+        }
+    }
+    
+    return steps
+};
